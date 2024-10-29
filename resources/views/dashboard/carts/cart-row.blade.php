fbd2ec6236f3470aad07a51b2bddcc91628085d0
Ibrahem-Khaled <xx.vvv48@gmail.com>
2024-10-29 20:48:21 +0200

    add can factor update status orders

--- resources/views/dashboard/carts/cart-row.blade.php
+++ resources/views/dashboard/carts/cart-row.blade.php
@@ -7,9 +7,19 @@
     <td>{{ $cart->car_color }}</td>
     <td>{{ $cart->car_number }}</td>
     <td>{{ $cart->car_wash }}</td>
-    <td>{{ $cart->status }}</td>
     <td>{{ $cart->car_type }}</td>
     <td>{{ $cart->price }}</td>
+    <td>
+        @if ($cart->status == 'acepted')
+            <span class="badge bg-success">مقبول</span>
+        @elseif ($cart->status == 'declined')
+            <span class="badge bg-danger">مرفوض</span>
+        @elseif ($cart->status == 'pending')
+            <span class="badge bg-info">قيد التنفيذ</span>
+        @else
+            <span class="badge bg-warning">غير مدفوع</span>
+        @endif
+    </td>
     <td>
         <button type="button" class="btn btn-warning" data-bs-toggle="modal"
             data-bs-target="#editCartModal{{ $cart->id }}">
