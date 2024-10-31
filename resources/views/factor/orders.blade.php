2124753f8ad09532b30eef8a3d21ebd8ef218d64
Ibrahem-Khaled <xx.vvv48@gmail.com>
2024-10-31 03:06:05 +0200

    improve code and fix bugs

--- resources/views/factor/orders.blade.php
+++ resources/views/factor/orders.blade.php
@@ -110,7 +110,7 @@
                                         <div class="mb-3">
                                             <p>تغيير حالة الطلب:</p>
 
-                                            @if ($item->status == 'pending')
+                                            @if ($item->status == 'pending' && auth()->user()->role == 'supervisor')
                                                 <button class="btn btn-success" name="status" value="pending"
                                                     type="submit">تحويل الطلب</button>
                                             @elseif ($item->status == 'pending')
